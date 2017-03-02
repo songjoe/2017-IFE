c97a45e31d94d061a802a1e3f15c1b7e98a0e94c
songjoe <928549103@qq.com>
2017-03-02 17:07:32 +0800

    Create main.js

---
+++ js-task5/main.js
@@ -0,0 +1,84 @@
+(function() {
+  var que = document.getElementById("que");
+  var inp = document.getElementById("inp");
+  var leftIn = document.getElementById("left-in");
+  var rightIn = document.getElementById("right-in");
+  var leftOut = document.getElementById("left-out");
+  var rightOut = document.getElementById("right-out");
+  var sortUp = document.getElementById("sort-up");
+  var n = 0;
+  leftIn.addEventListener("click",leftIN,false);
+  function leftIN() {
+    if(inp.value <= 100 && inp.value >= 10 && n <= 60){
+      var li = document.createElement("li");
+      li.style.height = inp.value + "px";
+      que.insertBefore(li,que.childNodes[0]);
+      n++;
+    }
+    else if(n > 60) {
+      alert("The queue is too long !");
+    }
+    else {
+      alert("Please input the number between 10 to 100 !")
+    }
+  }
+
+  rightIn.addEventListener("click",rightIN,false);
+  function rightIN() {
+    if(inp.value <= 100 && inp.value >= 10 && n <= 60){
+      var li = document.createElement("li");
+      li.style.height = inp.value + "px";
+      que.appendChild(li,que.childNodes[0]);
+      n++;
+    }
+    else if(n > 60) {
+      alert("The queue is too long !");
+    }
+    else {
+      alert("Please input the number between 10 to 100 !")
+    }
+    
+  }
+
+  leftOut.addEventListener("click",leftOUT,false);
+  function leftOUT() {
+    if(que.firstChild)
+    que.firstChild.remove();
+    n--;
+  }
+
+  rightOut.addEventListener("click",rightOUT,false);
+  function rightOUT() {
+    if(que.lastChild)
+    que.lastChild.remove();
+    n--;
+  }
+  
+  function sleep(numberMillis) {
+    var now = new Date();
+    var exitTime = now.getTime() + numberMillis;
+    while (true) {
+        now = new Date();
+        if (now.getTime() > exitTime)
+            return;
+    }
+  }
+
+  sortUp.addEventListener("click",sortUP,false);
+  function sortUP() {
+    var c = que.childNodes;
+    var tmp = 0;
+    var i;
+    var j;
+    for(j = 0; j < c.length -1; j++) {
+      for(i = 0; i < c.length - 1; i++) {
+        if (c[i].style.height >　c[i+1].style.height){
+          tmp = c[i].style.height;
+          c[i].style.height = c[i+1].style.height;
+          c[i+1].style.height = tmp;
+        }
+      }
+    }
+  }
+
+})();
